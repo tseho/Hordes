2eeeee8ae206402464f82d93a3d538d9615439e3
Quentin <quentin.favrie@gmail.com>
2013-06-30 22:14:25 +0200

    TargetPriority updated with new methods

--- src/hordes/mod/plugins/evolvedmobs/api/targets/TargetPriority.java
+++ src/hordes/mod/plugins/evolvedmobs/api/targets/TargetPriority.java
@@ -14,4 +14,22 @@ public enum TargetPriority {
     public boolean isHighterThan(TargetPriority otherPriority){
         return this.ordinal() > otherPriority.ordinal();
     }
+    
+    public TargetPriority getHighterPriority(){
+        for (int i = 0; i < TargetPriority.values().length; i++) {
+            if(this.isHighterThan(TargetPriority.values()[i])){
+                return TargetPriority.values()[i];
+            }
+        }
+        return this;
+    }
+    
+    public TargetPriority getLowerPriority(){
+        for (int i = TargetPriority.values().length - 1; i >= 0; i--) {
+            if(this.isHighterThan(TargetPriority.values()[i])){
+                return TargetPriority.values()[i];
+            }
+        }
+        return this;
+    }
 }
